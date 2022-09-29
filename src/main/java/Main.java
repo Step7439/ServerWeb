e909579d794a86d0945e18a255487b89218aa309
Step7439 <zamashikovs@mail.ru>
2022-09-29 22:16:45 +0900

    Edite main Handler

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,28 @@
+import com.sun.net.httpserver.Request;
+
+import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.logging.Handler;
 
 public class Main {
     final static int PORT = 9999;
 
     public static void main(String[] args) throws IOException {
         Server server = new Server();
+
+        // добавление handler'ов (обработчиков)
+        server.addHandler("GET", "/messages", new Handler() {
+            public void handle(Request request, BufferedOutputStream responseStream) {
+                // TODO: handlers code
+            }
+        });
+        server.addHandler("POST", "/messages", new Handler() {
+            public void handle(Request request, BufferedOutputStream responseStream) {
+                // TODO: handlers code
+            }
+        });
 
         server.setup(PORT);
     }
